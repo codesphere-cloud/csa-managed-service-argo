f19373e5b87fa9f0b4ee443a9760cff91fefca4b
Clemens Freitag <clemens@codesphere.com>
2025-09-03 09:51:45 +0000

    workaround kubeconfig

--- src/api/kubernetes.ts
+++ src/api/kubernetes.ts
@@ -11,9 +11,15 @@ const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 router.get("/", async (req, res) => {
   try {
     const namespace = 'default';
-    const response = await k8sApi.listNamespacedPod({ namespace });
-	const podNames = response.body?.items?.map(pod => pod.metadata.name) || [];
-    res.json(podNames);
+
+    k8sApi.listNamespacedPod({ namespace: namespace }).then((podList => {
+		
+		const podNames = podList.items?.map(pod => pod.metadata.name);
+    	console.log(podNames);
+
+	    res.json(podNames);
+    }));	  
+
   } catch (err) {
     console.error("Error fetching pods:", err);
     res.status(500).send("Error fetching pods");
